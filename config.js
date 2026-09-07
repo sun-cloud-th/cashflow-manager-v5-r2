d78f5bc6be700f922ab93fb539fa783b5eca0a4c
sun-cloud-th <sun.tyco@gmail.com>
2026-09-07 10:36:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.CASHFLOW_CONFIG = {clientId: 'YOUR_CLIENT_ID.apps.googleusercontent.com'};
+window.CASHFLOW_CONFIG = {clientId: '445504572426-k39hkruspi4rq59j3o63lkd4th3again.apps.googleusercontent.com'};
