a55265fc87c6b434e95236b521da1303f0af2efd
sun-cloud-th <sun.tyco@gmail.com>
2026-09-08 14:35:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.CASHFLOW_CONFIG = {
   // Keep the SAME OAuth client ID/project used by the previous release.
-  clientId:445504572426-k39hkruspi4rq59j3o63lkd4th3again.apps.googleusercontent.com.apps.googleusercontent.com',
+  clientId:'445504572426-k39hkruspi4rq59j3o63lkd4th3again.apps.googleusercontent.com'.apps.googleusercontent.com',
   authServer: 'https://cashflow-auth.sun-tyco.workers.dev'
 };
